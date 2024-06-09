7000edbc2121a7550e5af2c2a4f7e07bfe7a4080
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-06-10 00:09:00 +0900

    Update Solution.cs

--- 예상 대진표/Solution.cs
+++ 예상 대진표/Solution.cs
@@ -1,14 +1,13 @@
 public class Solution {
 
     public int solution(int n, int a, int b) {
-        int confrontDigit = (a - 1) ^ (b - 1);
+        int confrontingDigit = (a - 1) ^ (b - 1);
 
         int answer = 0;
         do
         {
-            confrontDigit >>= 1;
             answer++;
-        } while (0 < confrontDigit);
+        } while (confrontingDigit >> answer != 0);
 
         return answer;
     }
